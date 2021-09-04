49b6f55e1864429febd32ba8b49786b4863d4c89
SeoungKwang Kim <net0310@gmail.com>
2021-09-04 17:12:10 +0900

    솔리디티 데이터구조 샘플 제시.

--- contracts/Demo.sol
+++ contracts/Demo.sol
@@ -10,6 +10,38 @@ contract Demo {
   uint256 public constant INITIAL_SUPPLY = 1000 * 10**decimals;
   string constant test = "You can not see this";
 
+  // Array with all rooms
+  Room[] public rooms;
+
+  struct Room {
+    uint256 roomNumber;
+    string name;
+    string menu;
+  }
+
+  function createRoom(uint256 _roomNumber, string memory _name, string memory _menu) public returns(bool) {
+    Room memory newRoom;
+    newRoom.roomNumber = _roomNumber;
+    newRoom.name = _name;
+    newRoom.menu = _menu;
+    rooms.push(newRoom);
+    return true;
+  }
+
+  function getRoomsById(uint256 _roomId) public view returns(
+    string memory name,
+    string memory menu) {
+
+    Room memory room = rooms[_roomId];
+    return (
+    room.name,
+    room.menu);
+  }
+
+  function getRoomCounts() public view returns(uint) {
+    return rooms.length;
+  }
+
   function testing() public returns (string memory) {
     return "hello world";
   }
