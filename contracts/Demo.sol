37721617df7c1bd6032d3ec44f3c3e86a2ae89ab
SangIl-fnino <mosang1230@naver.com>
2021-09-04 14:56:42 +0900

    Create Function testing :

--- contracts/Demo.sol
+++ contracts/Demo.sol
@@ -1,10 +1,16 @@
 pragma solidity ^0.5.2;
+
 // solidity version
 
-contract Demo { // contract name is Demo
-    string public constant name = "DEMO token";
-    string public constant symbol = "DEMO";
-    uint public constant decimals = 18;
-    uint public constant INITIAL_SUPPLY = 1000 * 10 ** decimals;
-    string constant test = "You can not see this";
+contract Demo {
+  // contract name is Demo
+  string public constant name = "DEMO token";
+  string public constant symbol = "DEMO";
+  uint256 public constant decimals = 18;
+  uint256 public constant INITIAL_SUPPLY = 1000 * 10**decimals;
+  string constant test = "You can not see this";
+
+  function testing() public returns (string memory) {
+    return "hello world";
+  }
 }
