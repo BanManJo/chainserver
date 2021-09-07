fdaf0007631da92187ad93eb03382a8feca8f429
Suukkaa <86913290+Suukkaa@users.noreply.github.com>
2021-09-07 17:34:16 +0900

    OrderRoom struct

--- migrations/2_deploy_demo.js
+++ migrations/2_deploy_demo.js
@@ -3,3 +3,10 @@ const Demo2 = artifacts.require("Demo");
 module.exports = function(deployer){
     deployer.deploy(Demo2);
 };
+
+const OrderRoom2 = artifacts.require("OrderRoom");
+
+module.exports = function(deployer){
+    deployer.deploy(OrderRoom2);
+};
+
