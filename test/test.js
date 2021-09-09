2fd9f9cbf3707ef9a66407a0006cc1997a2d0c61
tjdrn0809 <tjdrn0917234@gmail.com>
2021-09-09 16:07:32 +0900

    ownerFetch 성공

--- test/test.js
+++ test/test.js
@@ -1,6 +1,5 @@
-const TransactionTest = artifacts.require('TransactionTest');
-const OrderRoom = artifacts.require('OrderRoom');
-
+const TransactionTest = artifacts.require("TransactionTest");
+const OrderRoom = artifacts.require("OrderRoom");
 
 // contract('TransactionTest', (accounts) => {
 //   it('컨트랙트 배포후 이더 전송 테스트', async () => {
@@ -16,28 +15,24 @@ const OrderRoom = artifacts.require('OrderRoom');
 //   });
 // });
 
-
-contract('OrderRoom', (accounts) => {
-  it('방만들기 동시에 방contract계좌 송금', async () => {
+contract("OrderRoom", (accounts) => {
+  it("방만들기 동시에 방contract계좌 송금", async () => {
     instance = await OrderRoom.deployed();
     balance = await web3.eth.getBalance(instance.address); // instance.address : contract 주소
 
-    await instance.createRoom("BBQ","original",1,10, { from: accounts[0], value: 10 });
+    await instance.createRoom("BBQ", "original", 1, 10, {
+      from: accounts[0],
+      value: 10,
+    });
     balance = await web3.eth.getBalance(instance.address);
-    assert.equal(balance, '10');
+    assert.equal(balance, "10");
 
-    await instance.orderComplete(0,{ from: accounts[0], value: 10 });
+    await instance.orderComplete(0, { from: accounts[0], value: 10 });
     balance = await web3.eth.getBalance(instance.address);
-    assert.equal(balance, '30');
+    assert.equal(balance, "20");
+
+    await instance.ownerFetch(0, 3);
+    balance = await web3.eth.getBalance(instance.address);
+    assert.equal(balance, "0");
   });
 });
-
-
-// contract('OrderRoom', (accounts) => {
-//   it('주문 클릭시 송금기능 테스트', async () => {
-//     instance = await OrderRoom.deployed();
-//     balance = await web3.eth.getBalance(instance.address); // instance.address : contract 주소
-//     assert.equal(balance, '0');
-    
-//   });
-// });
