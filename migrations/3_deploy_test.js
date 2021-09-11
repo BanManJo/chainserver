3b25f4fec1f89f38c9a6802f1b5b12d2c047e6c7
MoSangIl-mac <mosangil-mac@student.com>
2021-09-11 11:06:40 +0900

    Set code for deploying contracts on ropsten Network

--- migrations/3_deploy_test.js
+++ migrations/3_deploy_test.js
@@ -1,16 +1,15 @@
-const TransactionTest = artifacts.require("TransactionTest");
-const A = artifacts.require("A");
-const B = artifacts.require("B");
+// const TransactionTest = artifacts.require("TransactionTest");
+// const A = artifacts.require("A");
+// const B = artifacts.require("B");
 
-const AdminTest = artifacts.require("AdminTest");
-const ChickenHouseTest = artifacts.require("ChickenHouseTest");
-const OrderRoomTest = artifacts.require("OrderRoomTest");
+const AdminTest = artifacts.require('AdminTest');
+const ChickenHouseTest = artifacts.require('ChickenHouseTest');
+const OrderRoomTest = artifacts.require('OrderRoomTest');
 
 module.exports = function (deployer) {
-  deployer.deploy(TransactionTest);
-  deployer.deploy(A);
-  deployer.deploy(B);
-
+  // deployer.deploy(TransactionTest);
+  // deployer.deploy(A);
+  // deployer.deploy(B);
   deployer.deploy(AdminTest);
   deployer.deploy(ChickenHouseTest);
   deployer.deploy(OrderRoomTest);
