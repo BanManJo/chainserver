2bd2fc690db6461285ab8a611e127dd8175da79f
SangIl-fnino <mosang1230@naver.com>
2021-10-05 19:16:41 +0900

    Re Struct the code frame

--- contracts/ChickenHouseTest.sol
+++ contracts/ChickenHouseTest.sol
@@ -5,7 +5,6 @@ import "./Ownable.sol";
 
 // ChickenHouse 역할
 contract ChickenHouseTest is Ownable {
-  
   OrderRoomTest[] orderRooms;
 
   string storeName;
@@ -15,8 +14,11 @@ contract ChickenHouseTest is Ownable {
   Menu[] public menus;
   uint256 roomCount = 0;
 
-  enum FreshJuiceSize{ SUNSAL, BBUE, LARGE }
-
+  enum FreshJuiceSize {
+    SUNSAL,
+    BBUE,
+    LARGE
+  }
 
   struct Menu {
     string chickenName;
@@ -24,55 +26,81 @@ contract ChickenHouseTest is Ownable {
     uint256 menuState;
     uint256 menuIndex;
   }
-  
- constructor (string memory _storeName, string memory _latitude, string memory _longitude , address _owner) Ownable(_owner) public {
-          storeName = _storeName;
-          latitude = _latitude;
-          longitude = _longitude;
-      }
 
-  function getRoomsCount() public view returns(uint256){
-    return orderRooms.length;
-  }
+  event approveFinish(string _chickenName, uint256 _roomIndex);
 
-  function findOrderRoom(uint256 _roomIndex) public view returns(OrderRoomTest){
-          OrderRoomTest orderRoom = orderRooms[_roomIndex];
-          return orderRoom;
+  constructor(
+    string memory _storeName,
+    string memory _latitude,
+    string memory _longitude,
+    address _owner
+  ) public Ownable(_owner) {
+    storeName = _storeName;
+    latitude = _latitude;
+    longitude = _longitude;
   }
 
   // 메뉴(치킨, 가격)등록
-  function registerChickenHouse(string[] memory _chickenNames, uint256[] memory _prices, uint256[] memory _menuState) public {
-       require(_chickenNames.length == _prices.length);
+  function registerChickenHouse(
+    string[] memory _chickenNames,
+    uint256[] memory _prices,
+    uint256[] memory _menuState
+  ) public {
+    require(_chickenNames.length == _prices.length);
 
-        for(uint i = 0 ; i <_chickenNames.length ; i++){
-            menus.push(Menu(_chickenNames[i], _prices[i], _menuState[i], i));
-        }
-          
-      }
-      //메뉴 인덱스 값의 치킨이름, 가격 , state(뼈1, 순살2) 수정 함수
-      function setMenu(uint _index, string memory _chickenNames, uint256 _price, uint256 _menuState) public returns (bool) {
-          require(_index < menus.length);
-          
-          menus[_index].chickenName = _chickenNames;
-          menus[_index].price = _price;
-          menus[_index].menuState = _menuState;
-      }
-      // 메뉴 인덱스 삭제 후 맨뒤에 인덱스 해당 삭제된 인덱스로 가져오기 함수
-      function deleteMenu(uint256 _index) public returns (bool){
-          menus[_index] = menus[ menus.length-1];
-          menus[_index].menuIndex = _index;
-          delete menus[menus.length-1];
-          menus.length--;
-          
-      }
-    
+    for (uint256 i = 0; i < _chickenNames.length; i++) {
+      menus.push(Menu(_chickenNames[i], _prices[i], _menuState[i], i));
+    }
+  }
 
-      
-    
+  function getRoomsCount() public view returns (uint256) {
+    return orderRooms.length;
+  }
+
+  function findOrderRoom(uint256 _roomIndex)
+    public
+    view
+    returns (OrderRoomTest)
+  {
+    OrderRoomTest orderRoom = orderRooms[_roomIndex];
+    return orderRoom;
+  }
+
+  //메뉴 인덱스 값의 치킨이름, 가격 , state(뼈1, 순살2) 수정 함수
+  function setMenu(
+    uint256 _index,
+    string memory _chickenNames,
+    uint256 _price,
+    uint256 _menuState
+  ) public returns (bool) {
+    require(_index < menus.length);
+
+    menus[_index].chickenName = _chickenNames;
+    menus[_index].price = _price;
+    menus[_index].menuState = _menuState;
+  }
+
+  // 메뉴 인덱스 삭제 후 맨뒤에 인덱스 해당 삭제된 인덱스로 가져오기 함수
+  function deleteMenu(uint256 _index) public returns (bool) {
+    menus[_index] = menus[menus.length - 1];
+    menus[_index].menuIndex = _index;
+    delete menus[menus.length - 1];
+    menus.length--;
+  }
+
   // 주문방 만들기
-  function createRoom(uint256 _price, uint256 _finish, string memory _chickenName) public payable {
+  function createRoom(
+    uint256 _price,
+    uint256 _finish,
+    string memory _chickenName
+  ) public payable {
     // room 생성!!!!
-    OrderRoomTest orderRoom = new OrderRoomTest(_price, _finish, _chickenName);
+    OrderRoomTest orderRoom = new OrderRoomTest(
+      _price,
+      _finish,
+      _chickenName,
+      msg.sender
+    );
     orderRooms.push(orderRoom);
     // OrderRoomTest a = OrderRoomTest(address(uint160(address(orderRoom))));
     // a.blah.value(_valueToSend)(0,0);
@@ -80,51 +108,59 @@ contract ChickenHouseTest is Ownable {
     roomCount++;
   }
 
-  function() external payable {}
-  
-      
-   function matchRoom(string memory _chickenName, uint256 _roomIndex) public payable {
-      OrderRoomTest orderRoom = findOrderRoom(_roomIndex);
-     address(uint160(address(orderRoom))).transfer(msg.value);
-    orderRoom.matchRoom(_chickenName, _roomIndex);
-  }
-  
-  function approveOrder(string memory _chickenName, uint256 _roomIndex, address _address) public onlyOwner(_address)  {
-    OrderRoomTest orderRoom = findOrderRoom(_roomIndex); 
-    // require (tx.origin == owner);  
+  // function() external payable {}
+
+  function approveOrder(
+    string memory _chickenName,
+    uint256 _roomIndex,
+    address _address
+  ) public onlyOwner(_address) {
+    OrderRoomTest orderRoom = findOrderRoom(_roomIndex);
+    // require (tx.origin == owner);
     orderRoom.approveOrder(_chickenName, _roomIndex);
+
+    emit approveFinish(_chickenName, _roomIndex);
   }
 
-  function getBalance(uint256 _roomIndex) public view returns (uint256) {
+  // 방의 모인 금액을 가져옴
+  function getBalance(uint256 _roomIndex)
+    public
+    view
+    returns (uint256 _balance)
+  {
     OrderRoomTest orderRoom = findOrderRoom(_roomIndex);
-    return address(orderRoom).balance;
-
+    return _balance = address(orderRoom).balance;
   }
-
-   function getStateRoom(uint256 _roomIndex) public view returns (uint256) {
-    OrderRoomTest orderRoom = findOrderRoom(_roomIndex);
-    return orderRoom.getStateRoom();
-
-  }
-
 
   // 2. 원하는 치킨집의 정보를 치킨집 이름으로 가져온다?
-  function getChickenHouse() public view returns (string memory, string memory, string memory, uint8){
-    return (storeName, latitude, longitude, onOff); 
+  function getChickenHouse()
+    public
+    view
+    returns (
+      string memory _storeName,
+      string memory _latitude,
+      string memory _longitude,
+      uint8 _onOff
+    )
+  {
+    return (
+      _storeName = storeName,
+      _latitude = latitude,
+      _longitude = longitude,
+      _onOff = onOff
+    );
   }
-  
-  // 2.2 원하는 치킨집의 정보를 인덱스로 가져온다?
-  function getChickenHouse2() public view returns (string memory, string memory, string memory, uint8){
-    return (storeName, latitude, longitude, onOff); 
-  }
-
 
   // 2.5 원하는 치킨집의 메뉴를 가져온다?
-  function getStoreMenu() public view returns(string[] memory, uint256[] memory){
+  function getStoreMenu()
+    public
+    view
+    returns (string[] memory, uint256[] memory)
+  {
     string[] memory chickenName = new string[](menus.length);
-    uint256[] memory price = new uint256[](menus.length); 
-    
-    for(uint i = 0 ; i < menus.length; i++){
+    uint256[] memory price = new uint256[](menus.length);
+
+    for (uint256 i = 0; i < menus.length; i++) {
       chickenName[i] = menus[i].chickenName;
       price[i] = menus[i].price;
     }
@@ -132,60 +168,52 @@ contract ChickenHouseTest is Ownable {
   }
 
   // 2.5 다른방법
-    // function getStoreMenu2(uint256 _storeIndex) public view returns(Menu[] memory){
-    //   return menus;
-    // }
-
-
-  // 3. 원하는 주문방의 정보를 가져오기
-  function getRoomInfo(uint256 _roomIndex) public view returns (string memory, uint256, uint8, address){
-   OrderRoomTest orderRoom = findOrderRoom(_roomIndex);
-    return orderRoom.getRoomInfo();
-  }
+  // function getStoreMenu2(uint256 _storeIndex) public view returns(Menu[] memory){
+  //   return menus;
+  // }
 
   // onOff 변경 함수
-   function changeOnOff() public {
-      if(onOff == 0){
-        onOff = 1;
-      }else if(onOff == 1){
-        onOff = 0;
-      }
-   }
+  function changeOnOff() public {
+    if (onOff == 0) {
+      onOff = 1;
+    } else if (onOff == 1) {
+      onOff = 0;
+    }
+  }
 
-   // 하나의 메뉴를 추가하는 함수
-      function addOneMenu(string memory _chickenName, uint256 _price, uint256 _menuState) public { 
-         menus.push( Menu(_chickenName, _price, _menuState, menus.length - 1));
-
-   }
+  // 하나의 메뉴를 추가하는 함수
+  function addOneMenu(
+    string memory _chickenName,
+    uint256 _price,
+    uint256 _menuState
+  ) public {
+    menus.push(Menu(_chickenName, _price, _menuState, menus.length - 1));
+  }
 
   // 가게이름을 바꾸는 함수
   //  function changeStoreName(string memory _changeName) public {
   //    storeName = _changeName;
   //  }
 
-
   // location 변경 함수
-   function changeLocation(string memory _longitude, string memory _latitude) public {     
-     longitude = _longitude;
-     latitude = _latitude;
+  function changeLocation(string memory _longitude, string memory _latitude)
+    public
+  {
+    longitude = _longitude;
+    latitude = _latitude;
+  }
 
-   }
-  
   // user1이 돈을 넣고 시간이 초과되었을때 환불되는 함수
-    function refund1(uint256 _roomIndex) public {
-      OrderRoomTest orderRoom = findOrderRoom(_roomIndex);
-      orderRoom.refund1();
-      roomCount--;
-    }
+  function refund1(uint256 _roomIndex) public {
+    OrderRoomTest orderRoom = findOrderRoom(_roomIndex);
+    orderRoom.refund1();
+    roomCount--;
+  }
 
   // user1과 user2 에게 환불
-   function refund2(uint256 _roomIndex) public {
-      OrderRoomTest orderRoom = findOrderRoom(_roomIndex);
-      orderRoom.refund2();
-      roomCount--;
-    }  
-    
-
-   
-
+  function refund2(uint256 _roomIndex) public {
+    OrderRoomTest orderRoom = findOrderRoom(_roomIndex);
+    orderRoom.refund2();
+    roomCount--;
+  }
 }
--- contracts/OrderRoomTest.sol
+++ contracts/OrderRoomTest.sol
@@ -2,67 +2,79 @@ pragma solidity ^0.5.2;
 
 // OrderRoom 역할
 contract OrderRoomTest {
-
-  uint256 public price;
-  string public chickenName;
-  uint8 public state;
-  uint256 public startTime;
-  uint256 public finishTime;
-  address public user1;
-  address public user2;
+  uint256 private price;
+  string private chickenName;
+  uint8 private state;
+  uint256 private startTime;
+  uint256 private finishTime;
+  address private user1;
+  address private user2;
 
   event matchFinish(string _chickenName, uint256 _roomIndex);
-  event approveFinish(string _chickenName, uint256 _roomIndex);
 
   function() external payable {}
 
-      constructor(uint256 _price, uint256 _finish, string memory _chickenName) public {
-          price = _price;
-          chickenName = _chickenName;
-          state = 1;
-          startTime = now;
-          finishTime = now;
-          user1 = tx.origin;
-        
-      }
+  constructor(
+    uint256 _price,
+    uint256 _finish,
+    string memory _chickenName,
+    address _user1
+  ) public {
+    price = _price;
+    chickenName = _chickenName;
+    state = 1;
+    startTime = now;
+    finishTime = now;
+    user1 = _user1;
+  }
+
   // function blah(int x, int y) payable public {}
 
-  function matchRoom(string memory _chickenName, uint256 _roomIndex) public {
-      state = 2;
-      user2 = tx.origin;
+  function matchRoom(uint256 _roomIndex) public payable {
+    state = 2;
+    user2 = msg.sender;
 
-      emit matchFinish(_chickenName,  _roomIndex);
-      
+    emit matchFinish(chickenName, _roomIndex);
   }
 
-  function approveOrder(string memory _chickenName,  uint256 _roomIndex) public payable{
-    tx.origin.transfer(address(this).balance);  
+  function approveOrder(string memory _chickenName, uint256 _roomIndex)
+    public
+    payable
+  {
+    tx.origin.transfer(address(this).balance);
     state = 3;
-    
-
-    emit approveFinish(_chickenName,  _roomIndex);
-
-    
   }
 
-  function getStateRoom() external view returns (uint){
-
-    return state;
+  function getStateRoom() external view returns (uint256 _state) {
+    return _state = state;
   }
 
-  function getRoomInfo() public view returns (string memory, uint256, uint8, address){
-    return (chickenName, price, state, user1);
+  function getRoomInfo()
+    public
+    view
+    returns (
+      string memory _chickenName,
+      uint256 _price,
+      uint8 _state,
+      address _user1
+    )
+  {
+    return (
+      _chickenName = chickenName,
+      _price = price,
+      _state = state,
+      _user1 = user1
+    );
   }
 
   // user1이 돈을 넣고 시간이 초과되었을때 환불되는 함수
   function refund1() public {
-      address(uint160(address(user1))).transfer(address(this).balance);  
-      
+    address(uint160(address(user1))).transfer(address(this).balance);
   }
-  
+
   // user1과 user2 에게 환불
-    function refund2() public {
-     address(uint160(address(user1))).transfer(address(this).balance / 2);  
-      address(uint160(address(user2))).transfer(address(this).balance);
-  }    
+  function refund2() public {
+    address(uint160(address(user1))).transfer(address(this).balance / 2);
+    address(uint160(address(user2))).transfer(address(this).balance);
+  }
 }
